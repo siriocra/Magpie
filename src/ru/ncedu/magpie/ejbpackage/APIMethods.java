7819dcc00efb2aeae5072255deaadc41d89b800a
Irina Brovar <245596436@rambler.ru>
2012-04-21 00:39:06 +0400

    Local bean

--- src/ru/ncedu/magpie/ejbpackage/APIMethods.java
+++ src/ru/ncedu/magpie/ejbpackage/APIMethods.java
@@ -3,13 +3,13 @@ package ru.ncedu.magpie.ejbpackage;
 import java.net.URISyntaxException;
 import java.util.Collection;
 
-import javax.ejb.Remote;
+import javax.ejb.Local;
 
 import ru.ncedu.magpie.basicClasses.VKEvent;
 import ru.ncedu.magpie.basicClasses.VKUser;
 
-@Remote
-public interface APIMethodsRemote {
+@Local
+public interface APIMethods {
 	public VKUser getUserInfo(String accessToken, String userId) throws URISyntaxException;
 	public Collection<VKUser> getFriends(String accessToken, String userId) throws URISyntaxException;
 	public Collection<VKEvent> getEvents(String accessToken, String userId) throws URISyntaxException;
